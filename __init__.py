ff036bd75948d2e3c9c2e7b013f026fa1e502879
Spencer Magnusson <semagnum@gmail.com>
2024-10-27 14:55:36 -0600

    Add dynamic reloading of submodules

--- __init__.py
+++ __init__.py
@@ -15,6 +15,39 @@ Created by Spencer Magnusson
 """
 
 
+if "bpy" in locals():
+    import importlib
+    import os
+    import sys
+    import types
+
+    def reload_package(package):
+        assert (hasattr(package, '__package__'))
+        fn = package.__file__
+        fn_dir = os.path.dirname(fn) + os.sep
+        module_visit = {fn}
+        del fn
+
+        def reload_recursive_ex(module):
+            module_iter = (
+                module_child
+                for module_child in vars(module).values()
+                if isinstance(module_child, types.ModuleType)
+            )
+            for module_child in module_iter:
+                fn_child = getattr(module_child, '__file__', None)
+                if (fn_child is not None) and fn_child.startswith(fn_dir) and fn_child not in module_visit:
+                    # print('Reloading:', fn_child, 'from', module)
+                    module_visit.add(fn_child)
+                    reload_recursive_ex(module_child)
+
+            importlib.reload(module)
+
+        return reload_recursive_ex(package)
+
+    reload_package(sys.modules[__name__])
+
+
 import bpy
 
 from .operators.IMAGE_OT_screen_rect import IMAGE_OT_screen_rect
